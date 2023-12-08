96b6d41c611e7e3a44edd63936a6e49044a5250f
ouhammmourachid <93659459+ouhammmourachid@users.noreply.github.com>
2023-12-08 16:14:06 +0000

    Add console log and variable declaration in App.js, update README.md with setup instructions

--- App.js
+++ App.js
@@ -2,9 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log("App executed");
+  let x = 1;
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello my name is rachid ouhammou</Text>
       <StatusBar style="auto" />
     </View>
   );
--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 
 1. install expo-cli with `npm i -g expo-cli`.
 2. create a new project `expo init DoneWithIt`.
+3. start the app by `npm start` localy and `npm start --tunnel` on the cloud.
